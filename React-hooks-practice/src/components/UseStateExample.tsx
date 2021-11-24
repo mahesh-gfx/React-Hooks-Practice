47c31c269d783923784cd1b869aa40f81d5b86db
Mahesh Adhikari <mahesh.adhikari@cesltd.com>
2021-11-24 23:33:14 +0530

    Added useState Examples

---
+++ React-hooks-practice/src/components/UseStateExample.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const UseStateExample = () => {
+  const [counter, setCounter] = useState(0);
+  const [toggle, toggleSetter] = useState("false");
+  const [name, setName] = useState("");
+  const [personObject, setPerson] = useState({});
+
+  const _onClick = () => {
+    setCounter(counter + 1);
+  };
+
+  const _toggle = () => {
+    if (toggle == "true") {
+      toggleSetter("false");
+    } else toggleSetter("true");
+  };
+
+  const _formSubmit = (event: any) => {
+    event.preventDefault();
+    const nameData = event.target.name.value;
+    const ageData = event.target.age.value;
+    let item = [{ ...personObject }, { nameData, ageData }];
+    setPerson(item);
+    console.log("List:", personObject);
+  };
+
+  return (
+    <>
+      <label htmlFor="counter">Counter</label>
+      <button name="counter" onClick={_onClick}>
+        {counter}
+      </button>
+      <br />
+      <label htmlFor="toggle">Toggle</label>
+      <button name="toggle" onClick={_toggle}>
+        {toggle}
+      </button>
+      <form action="return 0;" onSubmit={_formSubmit}>
+        <label htmlFor="name">
+          <input type="text" name="name" placeholder="Name" />
+        </label>
+        <label htmlFor="age">
+          <input type="number" name="age" placeholder="Age" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+
+      <p>{name}</p>
+    </>
+  );
+};
+
+export default UseStateExample;
